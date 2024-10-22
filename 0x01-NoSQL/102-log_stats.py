390db0f6264fed9672ca8b94377538bd9719d5b1
Fares <faresyigeremu@gmail.com>
2024-10-22 13:38:30 -0700

    done with logs

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Task 15's module.
+'''adding the top 10 of the most present IPs in the collection nginx
 '''
 from pymongo import MongoClient
 
@@ -32,3 +32,23 @@ def print_top_ips(server_collection):
                 '$sort': {'totalRequests': -1}
             },
             {
+                '$limit': 10
+            },
+        ]
+    )
+    for request_log in request_logs:
+        ip = request_log['_id']
+        ip_requests_count = request_log['totalRequests']
+        print('\t{}: {}'.format(ip, ip_requests_count))
+
+
+def run():
+    '''Provides some stats about Nginx logs stored in MongoDB.
+    '''
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    print_nginx_request_logs(client.logs.nginx)
+    print_top_ips(client.logs.nginx)
+
+
+if __name__ == '__main__':
+    run()
