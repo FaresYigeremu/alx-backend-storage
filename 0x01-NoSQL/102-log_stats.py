61af87ded4f6f7b27b7e6581c0daeb029e8bea65
Fares <faresyigeremu@gmail.com>
2024-10-22 13:02:54 -0700

    adjusted the log stats

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -32,23 +32,3 @@ def print_top_ips(server_collection):
                 '$sort': {'totalRequests': -1}
             },
             {
-                '$limit': 10
-            },
-        ]
-    )
-    for request_log in request_logs:
-        ip = request_log['_id']
-        ip_requests_count = request_log['totalRequests']
-        print('\t{}: {}'.format(ip, ip_requests_count))
-
-
-def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
-    print_top_ips(client.logs.nginx)
-
-
-if __name__ == '__main__':
-    run()
